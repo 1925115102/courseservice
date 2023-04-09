b9b5f7966d214b9bcf1c99287d46014f0df7e6b1
pangjingwen <pangjing@iu.edu>
2023-04-09 16:35:16 -0400

    course

--- src/main/java/edu/iu/c322/project/courseservice/repository/CourseRepository.java
+++ src/main/java/edu/iu/c322/project/courseservice/repository/CourseRepository.java
@@ -1,8 +1,8 @@
 package edu.iu.c322.project.courseservice.repository;
 
-import edu.iu.c322.project.courseservice.model.Student;
+import edu.iu.c322.project.courseservice.model.Course;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CourseRepository extends JpaRepository<Student, Integer>{
+public interface CourseRepository extends JpaRepository<Course, Integer>{
 
 }
